246f1bfc28c2c87ad92b94e626c6bff392704902
I.Tsepas <I.Tsepas@deddie.gr>
2025-05-06 09:48:03 +0300

    added dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,16 +2,20 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.hilt)
+    alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.detekt)
 }
 
 android {
     namespace = "gts.trackmypath"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "gts.trackmypath"
         minSdk = 30
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
@@ -36,8 +40,18 @@ android {
     }
 }
 
+detekt {
+    source.setFrom("src/main/java", "src/main/kotlin")
+    config.setFrom("$rootDir/config/detekt/detekt.yml")
+    allRules = true
+    autoCorrect = true
+    parallel = true
+    buildUponDefaultConfig = true
+}
+
 dependencies {
 
+    implementation(libs.kotlin.coroutines)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -46,11 +60,27 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.hilt.android)
+    implementation(libs.androidx.room.ktx)
+    implementation(libs.kotlinx.serialization)
+    implementation(libs.hilt.navigation.compose)
+    implementation(libs.kotlinx.collections)
+
+    ksp(libs.hilt.compiler)
+    ksp(libs.androidx.room.compiler)
+
+    detektPlugins(libs.detekt.formatting)
+    detektPlugins(libs.detekt.compose.rules)
+
     testImplementation(libs.junit)
+    testImplementation(libs.kotlin.coroutines.test)
+    testImplementation(libs.androidx.room.tesing)
+
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.ui.test.junit4)
+
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
 }
