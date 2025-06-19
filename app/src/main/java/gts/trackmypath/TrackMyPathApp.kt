1148b1b726abc63388da49acbdb9cecb00e96815
gs-ts <john87ts@gmail.com>
2025-06-19 11:31:10 +0300

    set up hilt

---
+++ app/src/main/java/gts/trackmypath/TrackMyPathApp.kt
@@ -0,0 +1,7 @@
+package gts.trackmypath
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class TrackMyPathApp : Application()
--- app/src/main/java/gts/trackmypath/ui/MainActivity.kt
+++ app/src/main/java/gts/trackmypath/ui/MainActivity.kt
@@ -1,4 +1,4 @@
-package gts.trackmypath
+package gts.trackmypath.ui
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -11,8 +11,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import dagger.hilt.android.AndroidEntryPoint
 import gts.trackmypath.ui.theme.TrackMyPathV2Theme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
