ac8dd3e113691c8be0ce279314e519b8d7591dd4
gs-ts <john87ts@gmail.com>
2025-06-19 17:18:42 +0300

    added state in ActivePathViewModel and added fab for tracking path

--- app/src/main/java/gts/trackmypath/ui/activepath/ActivePathScreen.kt
+++ app/src/main/java/gts/trackmypath/ui/activepath/ActivePathScreen.kt
@@ -3,22 +3,39 @@ package gts.trackmypath.ui.activepath
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material.icons.filled.Stop
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import gts.trackmypath.ui.activepath.ActivePathViewModel.State.TrackingState
 
 @Composable
 fun ActivePathScreen(viewModel: ActivePathViewModel) {
-    ActivePathContent()
+    val state by viewModel.state.collectAsStateWithLifecycle()
+
+    ActivePathContent(
+        trackingState = state.trackingState,
+        onTrackPathClick = viewModel::onTrackPathClick,
+    )
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-private fun ActivePathContent(modifier: Modifier = Modifier) {
+private fun ActivePathContent(
+    modifier: Modifier = Modifier,
+    trackingState: TrackingState,
+    onTrackPathClick: () -> Unit = {},
+) {
     Scaffold(
         modifier = modifier,
         topBar = {
@@ -29,7 +46,22 @@ private fun ActivePathContent(modifier: Modifier = Modifier) {
             )
         },
         floatingActionButton = {
-            // TODO
+            FloatingActionButton(onClick = onTrackPathClick) {
+                val icon = if (trackingState == TrackingState.TRACKING) {
+                    Icons.Default.Stop
+                } else {
+                    Icons.Default.PlayArrow
+                }
+                val contentDescription = if (trackingState == TrackingState.TRACKING) {
+                    "Stop path tracking"
+                } else {
+                    "Start path tracking"
+                }
+                Icon(
+                    imageVector = icon,
+                    contentDescription = contentDescription,
+                )
+            }
         },
     ) { innerPadding ->
         Column(
--- app/src/main/java/gts/trackmypath/ui/activepath/ActivePathViewModel.kt
+++ app/src/main/java/gts/trackmypath/ui/activepath/ActivePathViewModel.kt
@@ -1,8 +1,42 @@
 package gts.trackmypath.ui.activepath
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import gts.trackmypath.ui.activepath.ActivePathViewModel.State.TrackingState
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class ActivePathViewModel @Inject constructor() : ViewModel()
+class ActivePathViewModel @Inject constructor() : ViewModel() {
+
+    private val _state = MutableStateFlow(State())
+    val state: StateFlow<State> = _state.asStateFlow()
+
+    fun onTrackPathClick() {
+        viewModelScope.launch {
+            _state.update { state ->
+                state.copy(
+                    trackingState = when (state.trackingState) {
+                        TrackingState.TRACKING -> TrackingState.STOPPED
+                        TrackingState.STOPPED -> TrackingState.TRACKING
+                    },
+                )
+            }
+        }
+    }
+
+    data class State(
+        val trackingState: TrackingState = TrackingState.STOPPED,
+    ) {
+
+        enum class TrackingState {
+            TRACKING,
+            STOPPED,
+        }
+    }
+}
