1c83b926b386b6b3c137d0a65c1eda3e8c60bac8
gs-ts <john87ts@gmail.com>
2025-08-17 16:01:45 +0300

    added LocationProvider

---
+++ app/src/main/java/gts/trackmypath/ui/LocationProvider.kt
@@ -0,0 +1,107 @@
+package gts.trackmypath.ui
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.location.Location
+import android.os.Looper
+import android.util.Log
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.Priority
+import dagger.hilt.android.qualifiers.ApplicationContext
+import gts.trackmypath.di.ApplicationScope
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.flow.shareIn
+import kotlinx.coroutines.suspendCancellableCoroutine
+import java.util.concurrent.TimeUnit
+import javax.inject.Inject
+import javax.inject.Singleton
+import kotlin.coroutines.resumeWithException
+
+@Singleton
+class LocationProvider @Inject constructor(
+    @ApplicationContext applicationContext: Context,
+    @ApplicationScope applicationScope: CoroutineScope
+) {
+
+    private val fusedLocationClient: FusedLocationProviderClient = LocationServices
+        .getFusedLocationProviderClient(applicationContext)
+
+    private val locationRequest = LocationRequest.Builder(
+        Priority.PRIORITY_HIGH_ACCURACY,
+        TimeUnit.SECONDS.toMillis(INTERVAL_TIME.toLong()),
+    )
+        .setMinUpdateIntervalMillis(TimeUnit.SECONDS.toMillis(FASTEST_INTERVAL_TIME.toLong()))
+        .setMinUpdateDistanceMeters(SMALLEST_DISPLACEMENT_100_METERS)
+        .build()
+
+    @SuppressLint("MissingPermission")
+    private val _locationUpdates = callbackFlow {
+        val locationCallback = object : LocationCallback() {
+
+            override fun onLocationResult(locationResult: LocationResult) {
+                locationResult.lastLocation?.let {
+                    Log.d("LocationProvider", "New location: ${it.latitude}, ${it.longitude}")
+                    trySend(it)
+                }
+            }
+        }
+
+        Log.d("LocationProvider", "Starting location updates")
+
+        val initialLocation = getLastKnownLocation()
+        initialLocation?.let {
+            Log.d("LocationProvider", "Initial last location: ${it.latitude}, ${it.longitude}")
+            trySend(initialLocation)
+        }
+
+        fusedLocationClient.requestLocationUpdates(
+            locationRequest,
+            locationCallback,
+            Looper.getMainLooper(),
+        ).addOnFailureListener { exception ->
+            Log.e("LocationProvider", "requestLocationUpdates failed: $exception")
+            close(exception) // in case of exception, close the Flow
+        }
+
+        awaitClose {
+            Log.d("LocationProvider", "Stopping location updates")
+            fusedLocationClient.removeLocationUpdates(locationCallback)
+        }
+    }.shareIn(
+        applicationScope,
+        replay = 0,
+        started = SharingStarted.WhileSubscribed(),
+    )
+
+    @SuppressLint("MissingPermission")
+    suspend fun getLastKnownLocation(): Location? = suspendCancellableCoroutine { continuation ->
+        fusedLocationClient.lastLocation
+            .addOnSuccessListener { location: Location? ->
+                continuation.resume(value = location) { cause, _, _ ->
+                    Log.d("LocationProvider", "lastLocation cancellation")
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.e("LocationProvider", "lastLocation failed: $exception")
+                continuation.resumeWithException(exception)
+            }
+    }
+
+    fun locationFlow(): Flow<Location> {
+        return _locationUpdates
+    }
+
+    companion object {
+        private const val SMALLEST_DISPLACEMENT_100_METERS = 60F
+        private const val INTERVAL_TIME = 30
+        private const val FASTEST_INTERVAL_TIME = 10
+    }
+}
