8ed212e09e3dcf65fba50f5b5253c6d4e4e4186f
gs-ts <john87ts@gmail.com>
2025-10-22 16:00:37 +0300

    renamed RepositoryModule to DataModule

--- app/src/main/java/gts/trackmypath/di/DataModule.kt
+++ app/src/main/java/gts/trackmypath/di/DataModule.kt
@@ -4,12 +4,19 @@ import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import gts.trackmypath.data.GooglePlacesClientImpl
+import gts.trackmypath.data.GooglePlacesClient
 import gts.trackmypath.data.PhotoRepositoryImpl
 import gts.trackmypath.domain.PhotoRepository
 
 @Module
 @InstallIn(SingletonComponent::class)
-interface RepositoryModule {
+interface DataModule {
+
+    @Binds
+    fun bindGooglePlacesClientI(
+        googlePlacesClientImpl: GooglePlacesClientImpl
+    ): GooglePlacesClient
 
     @Binds
     fun bindPhotoRepository(
