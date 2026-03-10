479612bba6a0ae8170fff639415e0af8c3c8ad73
gs-ts <john87ts@gmail.com>
2026-03-10 15:55:20 +0200

    updated gradle version

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,6 +11,16 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
+
+plugins {
+    // The foojay-resolver-convention plugin is simply the engine that tells Gradle where to download that missing JDK.
+    //
+    // If you (or a teammate, or a CI/CD server like GitHub Actions) try to build this project but
+    // don't have Java 17 installed, Gradle's "Toolchains" feature is smart enough to
+    // automatically download and install the correct JDK for you so the build doesn't fail.
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
