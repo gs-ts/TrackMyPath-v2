87aff4c29acc798603595bf0c7ed7cfc11c65304
gs-ts <john87ts@gmail.com>
2026-03-11 20:29:38 +0200

    updated PhotoRepositoryImpl to fetchPhotoMetadataForLocation and to observePhotos

--- app/src/main/java/gts/trackmypath/data/PhotoRepositoryImpl.kt
+++ app/src/main/java/gts/trackmypath/data/PhotoRepositoryImpl.kt
@@ -2,39 +2,94 @@ package gts.trackmypath.data
 
 import android.util.Log
 import com.google.android.gms.maps.model.LatLng
+import gts.trackmypath.data.database.photo.PhotoMetadataDao
+import gts.trackmypath.data.database.photo.PhotoMetadataEntity
+import gts.trackmypath.data.database.photo.toDomain
+import gts.trackmypath.data.network.GooglePlacesClient
+import gts.trackmypath.di.IoDispatcher
 import gts.trackmypath.domain.PhotoMetadata
 import gts.trackmypath.domain.PhotoRepository
+import gts.trackmypath.domain.RouteId
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 @Suppress("NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
 class PhotoRepositoryImpl @Inject constructor(
-    private val googlePlacesClient: GooglePlacesClient
+    private val googlePlacesClient: GooglePlacesClient,
+    private val photoMetadataDao: PhotoMetadataDao,
+    @param:IoDispatcher private val ioDispatcher: CoroutineDispatcher
 ) : PhotoRepository {
 
-    override suspend fun fetchPhotoMetadataForLocation(latLng: LatLng): Result<PhotoMetadata> {
-        val places = googlePlacesClient.searchNearbyPlaces(latLng)
+    override suspend fun fetchPhotoMetadataForLocation(
+        routeId: RouteId,
+        location: PhotoMetadata.Location
+    ): Result<Unit> {
+        return withContext(ioDispatcher) {
 
-        return if (places.isNotEmpty()) {
-            val firstPlace = places.first()
-            Log.d("PhotoRepository", "firstPlace received: ${firstPlace.id}")
-            firstPlace.id?.let { placeId ->
-                val photoUri = googlePlacesClient.fetchPhotoUri(
-                    photoMetadatas = firstPlace.photoMetadatas
+            val places = googlePlacesClient.searchNearbyPlaces(
+                latLng = LatLng(location.latitude, location.longitude)
+            )
+
+            if (places.isNotEmpty()) {
+                val firstPlace = places.first()
+                Log.d("PhotoRepository", "firstPlace id: ${firstPlace.id}")
+                Log.d("PhotoRepository", "firstPlace displayName: ${firstPlace.displayName}")
+                Log.d("PhotoRepository", "firstPlace googleMapsUri: ${firstPlace.googleMapsUri}")
+                Log.d(
+                    "PhotoRepository",
+                    "firstPlace generativeSummary: ${firstPlace.generativeSummary}"
+                )
+                Log.d(
+                    "PhotoRepository",
+                    "firstPlace neighborhoodSummary: ${firstPlace.neighborhoodSummary}"
                 )
 
-                photoUri?.let {
-                    Result.success(
-                        PhotoMetadata(
-                            id = placeId,
-                            photoUri = photoUri
+                firstPlace.id?.let { placeId ->
+                    val photoMetadata = firstPlace.photoMetadatas
+
+                    photoMetadata?.let {
+                        val photoUri = googlePlacesClient.fetchPhotoUri(
+                            photoMetadatas = firstPlace.photoMetadatas
                         )
-                    )
-                } ?: Result.failure(Exception("No photo available"))
-            } ?: Result.failure(Exception("No first place available"))
-        } else {
-            Log.e("GooglePlacesClient", "No places available")
-            // TODO improve exceptions
-            Result.failure(Exception("No places available"))
+                        photoUri?.let {
+                            photoMetadataDao.insert(
+                                photoMetadataEntity = PhotoMetadataEntity(
+                                    routeId = routeId.id,
+                                    placeId = placeId,
+                                    displayName = firstPlace.displayName,
+                                    location = PhotoMetadataEntity.Location(
+                                        latitude = location.latitude,
+                                        longitude = location.longitude
+                                    ),
+                                    photoUri = photoUri.toString(),
+                                    googleMapsUri = firstPlace.googleMapsUri?.toString(),
+                                    generativeSummary = firstPlace.generativeSummary?.overview,
+                                    neighborhoodSummary = firstPlace.neighborhoodSummary?.overview?.content
+                                )
+                            )
+                            Result.success(Unit)
+                        } ?: Result.failure(Exception("No photoUri available"))
+                    } ?: Result.failure(Exception("No photoMetadata available"))
+                } ?: Result.failure(Exception("No first place available"))
+            } else {
+                Log.e("GooglePlacesClient", "No places available")
+                // TODO improve exceptions
+                Result.failure(Exception("No places available"))
+            }
         }
+    }
+
+    override fun observePhotos(): Flow<List<PhotoMetadata>> {
+        return photoMetadataDao.observeAllPhotos()
+            .map { entities ->
+                entities.map { entity -> entity.toDomain() }
+            }
+            // This ensures the Flow only emits if the list content *actually* differs
+            // from the previous emission, preventing unnecessary UI redraws.
+            .distinctUntilChanged()
     }
 }
