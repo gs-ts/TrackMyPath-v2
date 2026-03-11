4067aa3ca48514a0d4a11e0a2f6ff73108c15ee1
gs-ts <john87ts@gmail.com>
2026-03-11 20:28:44 +0200

    added AppDatabase

---
+++ app/src/main/java/gts/trackmypath/data/database/AppDatabase.kt
@@ -0,0 +1,22 @@
+package gts.trackmypath.data.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import gts.trackmypath.data.database.converter.MetadataConverter
+import gts.trackmypath.data.database.photo.PhotoMetadataDao
+import gts.trackmypath.data.database.photo.PhotoMetadataEntity
+import gts.trackmypath.data.database.route.RouteDao
+import gts.trackmypath.data.database.route.RouteEntity
+
+@Database(
+    entities = [PhotoMetadataEntity::class, RouteEntity::class],
+    version = 1
+)
+@TypeConverters(MetadataConverter::class)
+abstract class AppDatabase : RoomDatabase() {
+
+    abstract fun photoMetadataDao(): PhotoMetadataDao
+
+    abstract fun routeDao(): RouteDao
+}
