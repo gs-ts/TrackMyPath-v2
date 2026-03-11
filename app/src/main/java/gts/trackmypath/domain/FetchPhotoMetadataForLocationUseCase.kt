b72c0b64f7297f92f67230087ffbc1372c67089f
gs-ts <john87ts@gmail.com>
2026-03-11 18:23:58 +0200

    updated FetchPhotoMetadataForLocationUseCase to pass route id

--- app/src/main/java/gts/trackmypath/domain/FetchPhotoMetadataForLocationUseCase.kt
+++ app/src/main/java/gts/trackmypath/domain/FetchPhotoMetadataForLocationUseCase.kt
@@ -1,11 +1,16 @@
 package gts.trackmypath.domain
 
-import com.google.android.gms.maps.model.LatLng
 import javax.inject.Inject
 
 class FetchPhotoMetadataForLocationUseCase @Inject constructor(private val photoRepository: PhotoRepository) {
 
-    suspend operator fun invoke(latLng: LatLng): Result<PhotoMetadata> {
-        return photoRepository.fetchPhotoMetadataForLocation(latLng)
+    suspend operator fun invoke(
+        routeId: RouteId,
+        location: PhotoMetadata.Location
+    ): Result<Unit> {
+        return photoRepository.fetchPhotoMetadataForLocation(
+            routeId = routeId,
+            location = location
+        )
     }
 }
