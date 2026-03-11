1220de23202f655a6cbd8a3a1d4363e8e9c41237
gs-ts <john87ts@gmail.com>
2026-03-11 18:23:47 +0200

    added RouteRepository that will contain past tracked paths

---
+++ app/src/main/java/gts/trackmypath/domain/RouteRepository.kt
@@ -0,0 +1,19 @@
+package gts.trackmypath.domain
+
+interface RouteRepository {
+
+    // called when the user clicks "Play" and returns the id of the route
+    suspend fun startRoute(): RouteId
+
+    // called when the user clicks "Stop" and adds a name and metadata if any
+    suspend fun finishRoute(
+        routeId: RouteId,
+        displayName: String,
+        metadata: Map<String, String>
+    )
+
+    suspend fun deleteRoute(routeId: RouteId)
+}
+
+@JvmInline
+value class RouteId(val id: Long)
