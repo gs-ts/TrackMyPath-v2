c00b8d6e2dcb2441b3ccb3e66ec02dc6a5c8000f
gs-ts <john87ts@gmail.com>
2026-03-31 18:11:26 +0300

    updated theme

--- app/src/main/java/gts/trackmypath/ui/theme/Color.kt
+++ app/src/main/java/gts/trackmypath/ui/theme/Color.kt
@@ -4,10 +4,132 @@ package gts.trackmypath.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+object ColorLightTokens {
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+    val Primary = Color(0xFF5B21B6)
+    val OnPrimary = Color(0xFFFFFFFF)
+    val PrimaryContainer = Color(0xFFEDE9FF)
+    val OnPrimaryContainer = Color(0xFF1A0050)
+    val InversePrimary = Color(0xFFCDB8FF)
+
+    val Secondary = Color(0xFF7C5CBF)
+    val OnSecondary = Color(0xFFFFFFFF)
+    val SecondaryContainer = Color(0xFFEFE5FF)
+    val OnSecondaryContainer = Color(0xFF280066)
+
+    val Tertiary = Color(0xFFB5468B)
+    val OnTertiary = Color(0xFFFFFFFF)
+    val TertiaryContainer = Color(0xFFFFD8EE)
+    val OnTertiaryContainer = Color(0xFF3E0024)
+
+    val Background = Color(0xFFFFFBFF)
+    val OnBackground = Color(0xFF1C1B1F)
+
+    val Surface = Color(0xFFFFFBFF)
+    val OnSurface = Color(0xFF1C1B1F)
+
+    val SurfaceVariant = Color(0xFFE7E0EC)
+    val OnSurfaceVariant = Color(0xFF49454F)
+
+    val InverseSurface = Color(0xFF312E37)
+    val InverseOnSurface = Color(0xFFF5EFFE)
+
+    val Error = Color(0xFFB3261E)
+    val OnError = Color(0xFFFFFFFF)
+    val ErrorContainer = Color(0xFFF9DEDC)
+    val OnErrorContainer = Color(0xFF410E0B)
+
+    val Outline = Color(0xFF79747E)
+    val OutlineVariant = Color(0xFFCAC4D0)
+
+    val Scrim = Color(0x66000000)
+
+    val SurfaceBright = Color(0xFFFFFBFF)
+    val SurfaceDim = Color(0xFFDED8E1)
+
+    val SurfaceContainerLowest = Color(0xFFFFFFFF)
+    val SurfaceContainerLow = Color(0xFFF7F2FA)
+    val SurfaceContainer = Color(0xFFF3EDF7)
+    val SurfaceContainerHigh = Color(0xFFECE6F0)
+    val SurfaceContainerHighest = Color(0xFFE6E0E9)
+
+    val PrimaryFixed = Color(0xFFEDE9FF)
+    val PrimaryFixedDim = Color(0xFFCDB8FF)
+    val OnPrimaryFixed = Color(0xFF1A0050)
+    val OnPrimaryFixedVariant = Color(0xFF5B21B6)
+
+    val SecondaryFixed = Color(0xFFEFE5FF)
+    val SecondaryFixedDim = Color(0xFFCDBDFF)
+    val OnSecondaryFixed = Color(0xFF280066)
+    val OnSecondaryFixedVariant = Color(0xFF7C5CBF)
+
+    val TertiaryFixed = Color(0xFFFFD8EE)
+    val TertiaryFixedDim = Color(0xFFEFB8D6)
+    val OnTertiaryFixed = Color(0xFF3E0024)
+    val OnTertiaryFixedVariant = Color(0xFFB5468B)
+}
+
+object ColorDarkTokens {
+
+    val Primary = Color(0xFFCDB8FF)
+    val OnPrimary = Color(0xFF1A0050)
+    val PrimaryContainer = Color(0xFF3F1699)
+    val OnPrimaryContainer = Color(0xFFEDE9FF)
+    val InversePrimary = Color(0xFF5B21B6)
+
+    val Secondary = Color(0xFFCDBDFF)
+    val OnSecondary = Color(0xFF400075)
+    val SecondaryContainer = Color(0xFF573E99)
+    val OnSecondaryContainer = Color(0xFFEFE5FF)
+
+    val Tertiary = Color(0xFFEFB8D6)
+    val OnTertiary = Color(0xFF5C1040)
+    val TertiaryContainer = Color(0xFF7C2D5E)
+    val OnTertiaryContainer = Color(0xFFFFD8EE)
+
+    val Background = Color(0xFF120C1E)
+    val OnBackground = Color(0xFFE6E0E9)
+
+    val Surface = Color(0xFF1C1427)
+    val OnSurface = Color(0xFFE6E0E9)
+
+    val SurfaceVariant = Color(0xFF2D2438)
+    val OnSurfaceVariant = Color(0xFFCAC4D0)
+
+    val InverseSurface = Color(0xFFE6E0E9)
+    val InverseOnSurface = Color(0xFF1C1427)
+
+    val Error = Color(0xFFF2B8B5)
+    val OnError = Color(0xFF601410)
+    val ErrorContainer = Color(0xFF8C1D18)
+    val OnErrorContainer = Color(0xFFF9DEDC)
+
+    val Outline = Color(0xFF948F9A)
+    val OutlineVariant = Color(0xFF2D2438)
+
+    val Scrim = Color(0x99000000)
+
+    val SurfaceBright = Color(0xFF2D2438)
+    val SurfaceDim = Color(0xFF120C1E)
+
+    val SurfaceContainerLowest = Color(0xFF0D0819)
+    val SurfaceContainerLow = Color(0xFF1C1427)
+    val SurfaceContainer = Color(0xFF221A2D)
+    val SurfaceContainerHigh = Color(0xFF2D2438)
+    val SurfaceContainerHighest = Color(0xFF382F44)
+
+    val PrimaryFixed = Color(0xFFEDE9FF)
+    val PrimaryFixedDim = Color(0xFFCDB8FF)
+    val OnPrimaryFixed = Color(0xFF1A0050)
+    val OnPrimaryFixedVariant = Color(0xFF5B21B6)
+
+    val SecondaryFixed = Color(0xFFEFE5FF)
+    val SecondaryFixedDim = Color(0xFFCDBDFF)
+    val OnSecondaryFixed = Color(0xFF280066)
+    val OnSecondaryFixedVariant = Color(0xFF7C5CBF)
+
+    val TertiaryFixed = Color(0xFFFFD8EE)
+    val TertiaryFixedDim = Color(0xFFEFB8D6)
+    val OnTertiaryFixed = Color(0xFF3E0024)
+    val OnTertiaryFixedVariant = Color(0xFFB5468B)
+}
