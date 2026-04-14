1bf3c62aad8cf2b6b3286e9e49f1dc77ad08000a
gs-ts <john87ts@gmail.com>
2026-04-14 16:41:46 +0300

    updated compileSdk and targetSdk to 37

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,12 +13,12 @@ plugins {
 
 configure<ApplicationExtension> {
     namespace = "gts.trackmypath"
-    compileSdk = 36
+    compileSdk = 37
 
     defaultConfig {
         applicationId = "gts.trackmypath"
         minSdk = 30
-        targetSdk = 36
+        targetSdk = 37
         versionCode = 1
         versionName = "1.0"
 
