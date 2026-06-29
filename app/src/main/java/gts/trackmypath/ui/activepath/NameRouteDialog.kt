eda94005dd80963d492b7391b1881aade9c23be8
gs-ts <john87ts@gmail.com>
2026-06-29 18:12:24 +0300

    fixed preview in NameRouteDialog

--- app/src/main/java/gts/trackmypath/ui/activepath/NameRouteDialog.kt
+++ app/src/main/java/gts/trackmypath/ui/activepath/NameRouteDialog.kt
@@ -1,9 +1,11 @@
 package gts.trackmypath.ui.activepath
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -109,11 +111,13 @@ internal fun NameRouteDialog(
 @Composable
 private fun NameRouteDialogPreview() {
     TrackMyPathV2Theme {
-        NameRouteDialog(
-            routeName = "my route",
-            onRouteNameChange = {},
-            onConfirmClick = {},
-            onDismissClick = {},
-        )
+        Box (modifier = Modifier.fillMaxSize()){
+            NameRouteDialog(
+                routeName = "my route",
+                onRouteNameChange = {},
+                onConfirmClick = {},
+                onDismissClick = {},
+            )
+        }
     }
 }
